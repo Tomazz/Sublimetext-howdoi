bc53809ddb71dc274887619c88d68a96f538aec6
Tomasz Sadowski <1106113s@student.gla.ac.uk>
2014-07-08 11:19:59 +0100

    updated SOAvgUsage. Done a bit of refactoring and bug fixing

--- SOAvgUsage.py
+++ SOAvgUsage.py
@@ -4,13 +4,23 @@ import stackexchange
 from stackauth import StackAuth
 import json
 import os
-import simpledate
+import datetime
+from dateutil.relativedelta import relativedelta
+import fleming
+import pygal
 
 sys.path.append('.')
 site = stackexchange.Site(stackexchange.StackOverflow,"wZjX24zp2Y3wv6THqhZyPQ((")
+#stackexchange.web.WebRequestManager.debug = True
 site.be_inclusive()
+date = datetime.datetime(2014,7,1)
+numberOfMonths = 3
+tagList = []
 
-tagList = ["Matlab","Fortran","R","Octave","Freemat"]
+
+with open("SOConfig.txt","r") as tagFile:
+    for line in tagFile:
+        tagList = line.split(",")
 
 #create the directory for the mined StackOverflow code snippets
 directory = os.getcwd() + "\\StackOverflow\\AverageCommunitySize"
@@ -24,29 +34,37 @@ infoDict["description"] = "this file contains information about the number of vi
 infoDict["content"] = {}
 
 for tag in tagList:
-	stackSearchResults = site.search(tagged=tag,fromdate = str(int(simpledate.SimpleDate('2014-04-01',tz='UTC').timestamp)),
-	todate=str(int(simpledate.SimpleDate('2014-04-10',tz='UTC').timestamp)))
-	total_count = 0
-	for question in stackSearchResults:
-		sys.stdout.flush()
-		#print(question)
-		#fetch all the data about the question
-		#question.fetch()
-		#print(question.view_count)
-		#sys.stdout.flush()
-		total_count += question.view_count
-		#counter = counter - 1
-		#if len(question.answers)>2:
-		#	for answer in question.answers:
-		#		if answer.json["is_accepted"] or answer.score > 12:
-		#			root = ET.fromstring("<StackOverflowXML>" + answer.body + "</StackOverflowXML>")
-		#			for element in root.iter(tag = "code"):
-		#				#print(element.text)
-		#				pass
-		#	print()
-		#	print(('{0:d} answers.'.format(len(question.answers))))
-	infoDict["content"][tag] = total_count
-	print("Total number of views for ----  {0}  ----is :".format(tag),total_count)
-	print()
+	infoDict["content"][tag] = 0
+
+
+for m in range(numberOfMonths):
+	fromDate = date - relativedelta(months= m + 1)
+	toDate = date - relativedelta(months= m)
+	print (fromDate,toDate)
+	for tag in tagList:
+		stackSearchResults = site.search(pagesize=100,tagged=tag,fromdate = str(fleming.unix_time(fromDate)),
+		todate=str(fleming.unix_time(toDate)))
+		total_count = 0
+		for question in stackSearchResults:
+			total_count += question.view_count
+
+		infoDict["content"][tag] += total_count
+		print("Total number of views for ----  {0}  ----is :".format(tag),total_count)
+		print()
+
+dataList = []
+for tag in tagList:
+	infoDict["content"][tag] = infoDict["content"][tag]/numberOfMonths
+	dataList += [infoDict["content"][tag]]
+
+
+###Chart
+bar_chart = pygal.Bar()
+bar_chart.title = 'Average community size'
+bar_chart.x_labels = tagList
+bar_chart.add('Average Number of Views', dataList)
+bar_chart.render_to_file(directory + "\\chart.svg")
+
+
 json.dump(infoDict,infoFile)
 infoFile.close()
